a83727dc932e06743acc14d8673767ce18b60ccd
nattapong2005 <azzzx30089@gmail.com>
2025-04-29 09:24:29 +0700

    add

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy import func
 from werkzeug.security import generate_password_hash, check_password_hash
 from model import *
 from datetime import datetime
-import time
+import os
 from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SUPERGAYBASS2025'  
@@ -386,4 +386,5 @@ def notfound(error):
     return "404 ไม่พบหน้าเพจ"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
